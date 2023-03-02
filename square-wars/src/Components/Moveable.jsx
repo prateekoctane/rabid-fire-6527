dcf6109be381a8ac223ff7b59dde752002972500
Saurav Mallik <sauravmallik786@gmail.com>
2023-03-03 01:10:17 +0530

    move

---
+++ square-wars/src/Components/Moveable.jsx
@@ -0,0 +1,42 @@
+  
+   import React, { useEffect, useState } from 'react'
+   
+   const Moveable = () => {
+
+    const [position, setPosition]  = useState({x : 0, y : 0})
+
+    useEffect(() =>{
+
+         function keyDownFun(e){
+            const move = 10;
+            if(e.code === 'ArrowRight'){
+
+                setPosition((prevPosition) => ({x : prevPosition.x + move, y : prevPosition.y}))
+            }else if(e.code === 'ArrowLeft'){
+
+                setPosition((prevPosition) => ({x : prevPosition.x -move, y : prevPosition.y}))
+            }else if(e.code === 'ArrowUp'){
+                setPosition((prevPosition) => ({x : prevPosition.x, y : prevPosition.y - move}))    
+        }else if(e.code === 'ArrowDown'){
+            setPosition((prevPosition) => ({x : prevPosition.x, y : prevPosition.y + move}))    
+    }
+}
+    document.addEventListener('keydown', keyDownFun)
+     
+    return ()=>{
+        document.removeEventListener('keydown', keyDownFun)
+    }
+},[])
+
+     return (
+         <div style={{ 
+            position: 'absolute', 
+            top: position.y, 
+            left: position.x, 
+            width : "100px",
+            height :"100px",
+            backgroundColor:"red"}}>
+    </div>
+     )
+   }
+   export default Moveable
